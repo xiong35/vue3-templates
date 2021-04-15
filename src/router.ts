cc341283fff15ee325e671aa7339b5e63f8110e7
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-04-15 14:28:24 +0800

    small fix

--- src/router.ts
+++ src/router.ts
@@ -1,37 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "./pages/Home.vue";
-import CreateRoom from "./pages/CreateRoom.vue";
-import JoinRoom from "./pages/JoinRoom.vue";
-import Play from "./pages/Play.vue";
-import Recon from "./pages/Recon.vue";
-import WaitRoom from "./pages/WaitRoom.vue";
 
 const routes = [
-  { path: "/", component: Home },
   {
-    path: "/createRoom",
-    name: "createRoom",
-    component: CreateRoom,
-  },
-  {
-    path: "/joinRoom",
-    name: "joinRoom",
-    component: JoinRoom,
+    path: "/",
+    name: "Home",
+    component: Home,
     props: (route: any) => ({
-      pw: route.query.pw,
-      number: route.query.number,
-    }),
-  },
-  { path: "/play", name: "play", component: Play },
-  { path: "/recon", name: "recon", component: Recon },
-  {
-    path: "/waitRoom",
-    name: "waitRoom",
-    component: WaitRoom,
-    props: (route: any) => ({
-      pw: route.query.pw,
-      number: route.query.number,
+      pw: route?.query?.pw,
+      number: route?.query?.number,
     }),
   },
 ];
